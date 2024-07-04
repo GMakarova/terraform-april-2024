8788599c168931d884e7002bd7fc80926c82b93e
GMakarova <galinamakarova3@gmail.com>
2024-07-04 05:51:38 +0000

    added Class2 files

---
+++ Class2/group.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_group" "developers1" {
+  name = "developers1"
+}
+
+resource "aws_iam_group" "developers2" {
+  name = "developers2"
+}
+
+resource "aws_iam_group" "developers3" {
+  name = "developers3"
+}
